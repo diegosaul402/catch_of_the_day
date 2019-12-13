a9a68c551eb7d152371cbe1ca95b79d4a60cf3b8
Diego Mendoza <diego_saul_402@hotmail.com>
2019-12-13 11:46:00 -0600

    Implement add to order functionality on fish item

--- catch-of-the-day/src/components/Fish.js
+++ catch-of-the-day/src/components/Fish.js
@@ -2,6 +2,10 @@ import React from 'react';
 import { formatPrice } from "../helpers";
 
 class Fish extends React.Component {
+  addFish = () => {
+    this.props.addToOrder(this.props.index);
+  };
+
   render() {
     const { image, name, price, desc, status } = this.props.details;
     const isAvailable = status === 'available';
@@ -9,10 +13,12 @@ class Fish extends React.Component {
       <li className="menu-fish">
         <img src={image} alt={name}/>
         <h3 className="fish-name">{name}
-        <span className="price">{formatPrice(price)}</span>
+          <span className="price">{formatPrice(price)}</span>
         </h3>
         <p>{desc}</p>
-        <button disabled={!isAvailable}>{isAvailable ? 'Add to Cart' : 'Sold Out!'}</button>
+        <button disabled={!isAvailable} onClick={this.addFish}>
+          {isAvailable ? 'Add to Cart' : 'Sold Out!'}
+        </button>
       </li>
     )
   }
