00218d9390f581dc94af63cfb1d152219b420f86
Diego Mendoza <diego_saul_402@hotmail.com>
2020-01-06 12:48:23 -0600

    Add localStorage for Order

--- catch-of-the-day/src/components/Order.js
+++ catch-of-the-day/src/components/Order.js
@@ -7,6 +7,9 @@ class Order extends React.Component {
     const count = this.props.order[key];
     const isAvailable = fish && fish.status === 'available';
 
+    // Make sure fish is loaded before we continue
+    if (!fish) return null;
+
     if(!isAvailable){
       return (
         <li key={key}>
